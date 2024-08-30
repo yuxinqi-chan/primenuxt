beaf61fa2a0886dcf137d4c5df73c9a3949a1d5d
yuxinqi <yuxinqichan@gmail.com>
2024-08-30 23:54:59 +0800

    nitro config add wasm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
   },
   nitro: {
     preset: "cloudflare-pages",
+    experimental: {
+      wasm: true,
+    },
   },
   modules: [
     "nitro-cloudflare-dev",
