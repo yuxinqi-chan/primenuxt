26f8359c32cc5cd468dee5ea7022152dda3d1690
yuxinqi <yuxinqichan@gmail.com>
2024-09-07 23:14:33 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-4">
+  <div v-auto-animate class="flex flex-col gap-4">
     <ArticleCardItem
       v-animateonscroll="{
         enterClass: 'animate-scalein',
