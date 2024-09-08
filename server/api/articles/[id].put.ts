c8ddc698a10f5b2ef9c4fe4c1974b76a4541b3ac
yuxinqi <yuxinqichan@gmail.com>
2024-09-08 12:19:53 +0800

    update

--- server/api/articles/[id].put.ts
+++ server/api/articles/[id].put.ts
@@ -46,7 +46,7 @@ export default defineEventHandler(async (event) => {
     if (part.filename && part.type && part.name) {
       const id = crypto.randomUUID();
       await media.setItemRaw(
-        `${id}/${part.filename}`,
+        `${id}:${part.filename}`,
         new File([part.data], part.filename, { type: part.type }),
         {
           httpMetadata: {
