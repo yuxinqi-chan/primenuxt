6720def15e87178d2d59062db6446b98514c3f9e
yuxinqi <yuxinqichan@gmail.com>
2024-09-14 19:33:07 +0800

    test

--- server/api/__sitemap__/test.ts
+++ server/api/__sitemap__/test.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   try {
-    const res = await $fetch("/api/__sitemap__/urls");
+    const res = await event.$fetch("/api/__sitemap__/urls");
     return res;
   } catch (error) {
     return JSON.stringify(error);
